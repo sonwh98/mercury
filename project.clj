4a07b4c0ba4aa217a34d18109a6fa5384f68c657
sto <son.c.to@gmail.com>
2021-07-10 00:06:59 +0700

    add dependencies

--- project.clj
+++ project.clj
@@ -1,4 +1,6 @@
 (defproject  stigmergy/mercury "0.1.3-SNAPSHOT"
   :source-paths ["src"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [org.clojure/core.async "1.3.618"]]
   :deploy-repositories [#_["releases" :clojars]
                         ["snapshots" :clojars]])
