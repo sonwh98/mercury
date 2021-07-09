21b22a187bf10787d6731cf78084d00ecab35623
sto <son.c.to@gmail.com>
2021-07-10 00:46:29 +0700

    bump up version number to 0.1.4-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject  stigmergy/mercury "0.1.3-SNAPSHOT"
+(defproject  stigmergy/mercury "0.1.4-SNAPSHOT"
   :source-paths ["src"]
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/core.async "1.3.618"]]
